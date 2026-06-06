d616e2892841c1b885b198ab0421a761813c3d38
Joey <joeyi@example.com>
2026-06-07 00:18:40 +0200

    Deploy site to docs folder

--- wedding-site/next.config.ts
+++ wedding-site/next.config.ts
@@ -2,7 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
-
+  basePath: "/wedding-website-",
+  assetPrefix: "/wedding-website-",
   turbopack: {
     root: __dirname,
   },
